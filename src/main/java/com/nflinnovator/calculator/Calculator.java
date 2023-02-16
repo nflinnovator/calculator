29b5702dc63cabecf47d1389fa6a8651e2b41c13
nflinnovator <nflinnovator@gmail.com>
2023-02-16 13:23:39 +0000

    Added caching with hazelcast

--- src/main/java/com/nflinnovator/calculator/Calculator.java
+++ src/main/java/com/nflinnovator/calculator/Calculator.java
@@ -1,9 +1,18 @@
 package com.nflinnovator.calculator;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+     @Cacheable("sum")
      public int sum(int a, int b) {
+	  try {
+             Thread.sleep(3000);
+          }
+          catch (InterruptedException e) {
+             e.printStackTrace();
+          }
           return a + b;
      }  
 }
